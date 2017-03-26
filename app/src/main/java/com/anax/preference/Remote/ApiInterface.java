344b9d9a9a0238d8f51e3a34a30d21976d191286
Anas Alaa <anas.alaa@drbridge.com>
2017-03-26 13:52:59 +0200

    first push

---
+++ app/src/main/java/com/anax/preference/Remote/ApiInterface.java
@@ -0,0 +1,11 @@
+package com.anax.preference.Remote;
+
+/**
+ * @author Anas Alaa
+ * @version 1.0.0
+ * @since 27/01/2017
+ */
+
+public interface ApiInterface {
+    String ENDPOINT = "https://gist.githubusercontent.com/anasanasanas/";
+}
---
+++ preferencerhythm/src/main/java/com/anax/preferencerhythm/PreferenceRepo.java
@@ -0,0 +1,26 @@
+package com.anax.preferencerhythm;
+
+import java.util.List;
+
+
+/**
+ * @author Anas Alaa
+ * @version 1.0.0
+ * @since 21/01/2017
+ */
+
+interface PreferenceRepo<T> {
+
+    List<T> getList();
+
+    void saveList(List<T> tList);
+
+    T getObject();
+
+    void saveObject(T t);
+
+    void clear();
+
+    boolean isEmpty();
+
+}
